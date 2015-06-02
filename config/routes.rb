fa163078a65ac007e986db4ede470101c1a7828f
subhajit007 <subhajitusa007@gmail.com>
2015-06-02 15:59:10 -0700

    added profile controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
-
+resources :users do
+    resource :profile
+  end
 resources :contacts
+
 get '/about' => 'pages#about'
   root 'pages#home'
 end
